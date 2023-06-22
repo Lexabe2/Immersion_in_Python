c88958155fa6e29f1d02cb14184eb55f6b821bca
Lexabe2 <127139145+Lexabe2@users.noreply.github.com>
2023-06-22 08:49:14 +0300

    Update Homework_assignments_2_task_3.py

--- Homework_assignments_2_task_3.py
+++ Homework_assignments_2_task_3.py
@@ -1,16 +1,16 @@
-# Напишите программу, которая принимает две строки вида “a/b” - дробь с 
-# числителем и знаменателем. Программа должна возвращать сумму и произведение* 
+# Напишите программу, которая принимает две строки вида “a/b” - дробь с
+# числителем и знаменателем. Программа должна возвращать сумму и произведение*
 # дробей. Для проверки своего кода используйте модуль fractions.
-a = "1/2"
-b = "1/3"
+a = input("Введите первую дробь в формате x/x: ")
+b = input("Введите вторую дробь в формате x/x: ")
 
 product_of_fractions = f"{int(a[0]) * int(b[0])}/{int(a[2]) * int(b[2])}"
 
-print(product_of_fractions)
+print(f"Произведение дробей: {product_of_fractions}")
 
 common_divisor = int(a[2]) * int(b[2])
 a = f"{int(a[0]) * common_divisor // int(a[2])}/{common_divisor}"
 b = f"{int(b[0]) * common_divisor // int(b[2])}/{common_divisor}"
 sum_of_fractions = f"{str(int(a[0]) + int(b[0]))}/{str(common_divisor)}"
 
-print(sum_of_fractions)
+print(f"Сложение дробей: {sum_of_fractions}")
