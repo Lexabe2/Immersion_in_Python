9ea99f8b112d799f6deb2376339a56d85f2ac803
Lexabe2 <127139145+Lexabe2@users.noreply.github.com>
2023-07-03 10:18:17 +0300

    Update Homework_assignments_5_task_2.py

--- Homework_assignments_5_task_2.py
+++ Homework_assignments_5_task_2.py
@@ -6,7 +6,6 @@
 # как ставка умноженная на процент премии
 
 
-def task_2(names: list[str], bets: list[int], bonus: list[str]) -> dict[
-                                                                   str:int]:
+def task_2(names: list[str], bets: list[int], bonus: list[str]) -> dict[str:int]:
     return {name: bet / 100 * float(bon) for name, bet, bon in
             zip(names, bets, map(lambda x: x.replace('%', ''), bonus))}
