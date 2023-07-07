91afcee42a914654a116ab53df6f40e4b53202af
Lexabe2 <127139145+Lexabe2@users.noreply.github.com>
2023-07-07 08:49:35 +0300

    Update calendar.py

--- mod/calendar.py
+++ mod/calendar.py
@@ -1,32 +1,29 @@
-from sys import argv as gv
+from sys import argv
 
-__all__ = ['date']
+__all__ = ['date_validate']
+
+data = argv[1]
+_LEAP = 4
+_END_DAY = 31
+_START_DAY = 1
+_END_MONTH = 12
+_START_MONTH = 1
+_END_YEAR = 9999
+_START_YEAR = 1
 
 
-def __year(number: int) -> bool:
-    if (number % 4 == 0 or number % 400 == 0) and number % 100 != 0:
+def date_validate(date: str) -> bool:
+    day, month, year = map(int, date.split('.'))
+    return ((_END_DAY >= day >= _START_DAY) and
+            (_END_MONTH >= month >= _START_MONTH) and
+            (_END_YEAR >= year >= _START_YEAR))
+
+
+def _is_leap_year(date: str) -> bool:
+    day, month, year = map(int, date.split('.'))
+    if year % _LEAP == 0:
         return True
     return False
 
-
-def date(date_str: str, bracket=gv[1:]) -> bool:
-    rezzul = []
-    if bracket:
-        d, m, y = [x for x in map(int, bracket)]
-    else:
-        d, m, y = [x for x in map(int, date_str.split('.'))]
-    if all((0 < d < 32, 0 < m < 13, 0 < y < 10000)):
-        if m in [1, 3, 5, 7, 8, 10, 12]:
-            rezzul.append(d <= 31)
-        elif m in [4, 6, 9, 11]:
-            rezzul.append(d <= 30)
-        else:
-            if __year(y):
-                rezzul.append(d == 29)
-            else:
-                rezzul.append(d == 28)
-    return all(rezzul)
-
-
 if __name__ == '__main__':
-    print(date('29.02.2001'))
+    print(date_validate(data))
