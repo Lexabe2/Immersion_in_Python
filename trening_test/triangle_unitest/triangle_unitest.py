2ba34b8ac10389662a7e908a1dae90940a8b39fc
Alekseu <natash79@inbox.ru>
2023-08-03 13:54:47 +0300

    1

---
+++ trening_test/triangle_unitest/triangle_unitest.py
@@ -0,0 +1,18 @@
+import unittest
+import triangle
+
+
+class TestSum(unittest.TestCase):
+    def setUp(self) -> None:
+        self.triangle_versatile = triangle.Triangle(1, 2, 3)
+        self.triangle_isosceles = triangle.Triangle(2, 2, 3)
+
+    def test_comparison(self):
+        self.assertEqual(self.triangle_versatile.check(),
+                         'Треугольник разносторонний')
+        self.assertEqual(self.triangle_isosceles.check(),
+                         'Треугольник равнобедренный')
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
